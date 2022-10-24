be0f64be1a93a9ad5fc7b65aeb027ca22febd4d5
PStewart1 <landofalcon007@gmail.com>
2022-10-24 16:54:50 -0500

    Wrote a script that prepares a MySQL server for the project

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for testing the console"""
+import unittest
+# from models.base_model import BaseModel
+# from models import storage
+import console
+import os
+
+
+class test_console(unittest.TestCase):
+    """ Class to test the console """
+
+    # def test_create(self):
+    #     """  """
