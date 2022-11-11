8f841b8c850157def14b47a74255d89fcad74786
PStewart1 <landofalcon007@gmail.com>
2022-11-11 11:38:53 -0600

    replaced '_' with ' '

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,7 +16,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def variable(text):
-    return 'C {}'.format(text)
+    return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
