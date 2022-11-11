353213c19c8d68b77c5872be86872f2263e02900
PStewart1 <landofalcon007@gmail.com>
2022-11-11 14:17:39 -0600

    small changes

--- models/state.py
+++ models/state.py
@@ -3,20 +3,24 @@
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City', backref='state', cascade="all, delete, delete-orphan")
 
-    @property
-    def cities(self):
-        from models import storage, city
-        dics = storage.all(city.City)
-        diclist = []
-        for v in dics.values():
-            if v.state_id == self.id:
-                diclist.append(v)
-        return diclist
+    else:
+        @property
+        def cities(self):
+            from models import storage, city
+            dics = storage.all(city.City)
+            diclist = []
+            for v in dics.values():
+                if v.state_id == self.id:
+                    diclist.append(v)
+            return diclist
