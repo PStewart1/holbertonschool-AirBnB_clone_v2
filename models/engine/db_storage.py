50f81c178ce87dd6ba0c3f5a7f39f8366cda6f6f
PStewart1 <landofalcon007@gmail.com>
2022-11-14 12:13:46 -0600

    getting rid of charset=latin1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -20,7 +20,7 @@ class DBStorage():
     def __init__(self):
         """ Starts the engine, and drops existing tables if in test env """
         self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}?charset=latin1'.format(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
                 getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
                 getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB')),
             pool_pre_ping=True)
